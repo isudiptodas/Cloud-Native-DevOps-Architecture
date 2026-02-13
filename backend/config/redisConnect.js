ac38446aee8d9ee73e64310caa54fe2f6c32f91f
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-02-13 14:03:06 +0530

    Update redisConnect.js

--- backend/config/redisConnect.js
+++ backend/config/redisConnect.js
@@ -1,9 +1,12 @@
 import { createClient } from 'redis';
 
 export const redisConnect = createClient({
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
     socket: {
         host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT,
     },
 });
+
 
